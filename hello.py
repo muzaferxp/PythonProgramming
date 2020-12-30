dfcfa91b92d241c8d0abcf420e8d8e1205c14536
Muzafer <muzafer.xp@gmail.com>
2020-12-30 15:51:23 +0530

    init

---
+++ hello.py
@@ -0,0 +1,84 @@
+
+#comment
+#print('Hello World')
+print("Welcome")
+print("How can i help you?")
+
+#======variables=====
+a = '10'
+a = 5
+
+print(type(a))
+print(a)
+
+#==rules to be followed for declaring avariable
+#it should not start with number (1,2..), special characters ($, @...)
+#it should not be a predefined keyword (if else def print)
+#variables names are case sensitive
+A1234 = "Adam"
+
+"""
+$da = 5
+
+if = "print hello"
+
+nullname
+
+fullname
+
+FullName
+
+full_name
+
+"""
+
+#==============DATA TYPES========
+#1 int - 10
+#2 str - "Adam", 'Adam'
+#3 bool - True, False
+#4 list - [] , [1,2,3,4,4, "a", "b" , "c"]
+#5 tuple - (), (1,2,3,4)
+#6 dict - {"key" : "value" }
+#7 set - {'value1', 'value2', }
+
+
+
+#list example
+list_data = [1,2,3,"a","b","c"]
+
+emp_data = [
+    {"name" : "Adam"},
+    {"name" : "Sam"},
+    {"name" : "David"},
+    {"name" : "Rohit"}
+    ]
+
+
+print(emp_data)
+
+set_data = {"Adam", "Samm", "Adam"}
+
+print("SET DATA: " , set_data)
+
+
+
+data = {
+       "name" : "Adam",
+       "age" : 20
+    }
+
+username = data["name"]
+
+print(username)
+
+
+
+
+
+
+
+
+
+
+
+
